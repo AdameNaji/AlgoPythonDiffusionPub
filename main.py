2cd4b569c29bf5e0bbe92988111781cdc22d0480
Adame Naji <adame.naji@etu.univ-nantes>
2023-01-30 10:23:13 +0100

    Algo du bandit Mais il y a un truc qui marche pas avec le compteur de clic totaux

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ def clic(pub):
     pub.probClicEff = pub.clic / pub.display
 
 
+def choose_ad(pubs):
+    prob_clic_eff_list = [pub.probClicEff for pub in pubs]
+    return pubs[prob_clic_eff_list.index(max(prob_clic_eff_list))]
+
+
 def full_random_choice(list_pub):
     nbr_clic = 0
 
@@ -51,7 +56,7 @@ def full_random_choice(list_pub):
     print('\n')
 
 
-def upper_min_theorical_choice(list_pub):
+def upper_min_theorical_choice(list_pub, minimum_click_through_rate):
     nbr_clic = 0
 
     for i in range(1, MAX_ITERATE):
@@ -63,7 +68,7 @@ def upper_min_theorical_choice(list_pub):
             clic(rand_pub)
             nbr_clic += 1
 
-        list_pub = list(filter(lambda x: x.probClicEff >= 0.5, list_pub))
+        list_pub = list(filter(lambda x: x.probClicEff >= minimum_click_through_rate, list_pub))
 
     list_pub = sorted(list_pub, key=lambda x: x.probClicEff)
 
@@ -77,5 +82,30 @@ def upper_min_theorical_choice(list_pub):
     print('\n')
 
 
+def bandit_algorithm(pubs):
+    nbr_clic = 0
+    selected_ad = 0
+
+    for i in range(MAX_ITERATE):
+        selected_ad = random.choice(pubs)
+        selected_ad.display += 1
+        if random.random() < selected_ad.probClicTh:
+            selected_ad.clic += 1
+            nbr_clic += 1
+
+        selected_ad.probClicEff = selected_ad.clic / selected_ad.display
+        selected_ad = choose_ad(pubs)
+
+    print("========== SELECTION PAR ALGORITHME DU BANDIT ==========\n")
+
+    print(selected_ad.name + ' : ' + str(selected_ad.probClicEff) + ' clic effectif pour une proba de clic théorique de ' +
+          str(selected_ad.probClicTh) + ' - ' + str(selected_ad.clic) + ' clics realisés')
+
+    print('Nombre de clic total : ' + str(nbr_clic))
+    print('\n')
+
+
 full_random_choice(pubs)
-upper_min_theorical_choice(pubs)
+upper_min_theorical_choice(pubs, 0.5)
+# upper_min_theorical_choice(pubs, 0.7)
+bandit_algorithm(pubs)
